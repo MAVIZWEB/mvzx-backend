2297a135eeaa3d0eaa2e520adda65bfd37ee0a69
MAZOLX <firmwealthinfo@gmail.com>
2025-08-16 13:03:07 +0100

    Create walletService.ts

---
+++ src/services/walletService.ts
@@ -0,0 +1,9 @@
+export function calculateMatrixPosition(userId: number) {
+  return {
+    stage: 1,
+    position: 5,
+    expectedEarnings: 2000,
+    earningsSoFar: 1000,
+    earningsLeft: 1000
+  };
+}
