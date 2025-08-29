8e813262ea269e5fb6bafd4c95cbb7b5d467ff47
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-29 21:57:00 +0100

    Delete src/utils/hashPin.ts

--- src/utils/hashPin.ts
+++
@@ -1,9 +0,0 @@
- import bcrypt from "bcryptjs";
-import { env } from "./env";
-
-export function hashPin(pin: string) {
-  return bcrypt.hashSync(pin + env.PIN_SALT, 10);
-}
-export function verifyPin(pin: string, hash: string) {
-  return bcrypt.compareSync(pin + env.PIN_SALT, hash);
-}
