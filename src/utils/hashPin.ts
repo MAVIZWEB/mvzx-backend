465cf9188da60372f164d75bc5c7401305cee7d4
MAVIZWEB <firmwealthinfo@gmail.com>
2025-08-29 06:17:35 +0100

    Update hashPin.ts

--- src/utils/hashPin.ts
+++ src/utils/hashPin.ts
@@ -1,9 +1,9 @@
  import bcrypt from "bcryptjs";
+import { env } from "./env";
 
-export function hashPin(pin: string, salt: string) {
-  return bcrypt.hashSync(pin + salt, 10);
+export function hashPin(pin: string) {
+  return bcrypt.hashSync(pin + env.PIN_SALT, 10);
 }
-
-export function verifyPin(pin: string, salt: string, hash: string) {
-  return bcrypt.compareSync(pin + salt, hash);
+export function verifyPin(pin: string, hash: string) {
+  return bcrypt.compareSync(pin + env.PIN_SALT, hash);
 }
