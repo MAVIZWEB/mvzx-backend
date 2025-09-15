a99b480a78bceacf79bd1ede38182337647965bd
MAVIZWEB <firmwealthinfo@gmail.com>
2025-09-15 12:30:31 +0100

    Delete back directory

--- back/src/utils/crypto.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from 'bcrypt';
-const SALT = process.env.PIN_SALT || 'amabeth 41';
-export async function hashPin(pin: string) {
-  return bcrypt.hash(pin + SALT, 10);
-}
-export async function comparePin(pin:string, hash:string) {
-  return bcrypt.compare(pin + SALT, hash);
-}
